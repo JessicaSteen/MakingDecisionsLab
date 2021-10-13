9c5ee57360497f7b12b012533ab83445f13a8b46
Jessica Steen <jessicalsteen@yahoo.com>
2021-10-13 14:35:56 -0600

    Commit 3

--- lab.js
+++ lab.js
@@ -18,6 +18,7 @@ if (lovesCode = true) {
 
 // For problems 2-3 use the following lines of code:
 var amysAge = 29
+
 var brittanisAge = 34
 var amysBirthYear = 1991
 var brittanisBirthYear = 1986
@@ -25,11 +26,17 @@ var brittanisBirthYear = 1986
 ////////// PROBLEM 2 //////////
 
 /*
-  Using an if statement and a comparison operator, create code that will check if Amy or Brittani is older, and then console.log '{name} is older'. Hint: Consider what will happen if they are the same age. Handle this situation.
+  Using an if statement and a comparison operator, create code that will check if Amy or Brittani is older, 
+  and then console.log '{name} is older'. Hint: Consider what will happen if they are the same age. 
+  Handle this situation.
 */
 
 //CODE HERE
-
+if (amysAge > brittanisAge) {
+  console.log("Amy is older")
+} else if (amysAge < brittanisAge) {
+  console.log("Brittani is older")
+}
 
 ////////// PROBLEM 3 //////////
 
